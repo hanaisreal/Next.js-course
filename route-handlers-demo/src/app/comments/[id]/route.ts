3c41fa5429bd3bda77bd9bd1d18529f06aac8705
hanaisreal <ohhanathefirst@gmail.com>
2024-02-29 14:54:15 +0900

    feat: patch, delete included

--- route-handlers-demo/src/app/comments/[id]/route.ts
+++ route-handlers-demo/src/app/comments/[id]/route.ts
@@ -7,3 +7,28 @@ export async function GET(
     parseInt(params.id));
     return Response.json(comment);
 }
+
+export async function PATCH(
+    request: Request, 
+    {params}: {params: {id: string}}) {
+    const comment = await request.json();
+    //find index of comment
+    const index = comments.findIndex((comment) => comment.id ===
+    parseInt(params.id));
+    //change comment
+    comments[index] = comment;
+    return Response.json(comment);
+}
+
+export async function DELETE(
+    request: Request, 
+    {params}: {params: {id: string}}
+    ) {
+    //find index of comment
+    const index = comments.findIndex((comment) => comment.id ===
+    parseInt(params.id));
+    //delete comment
+    comments.splice(index, 1);
+    return new Response(null, {status: 204});
+}
+
