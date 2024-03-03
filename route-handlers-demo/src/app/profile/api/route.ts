40f63e1960211049606c24514bdc48f99e7b0336
hanaisreal <ohhanathefirst@gmail.com>
2024-03-03 16:41:19 +0900

    feat: route handling with cookie and headers

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,3 +1,19 @@
-export async function GET() {
-    return new Response('Profile data');
+import { type NextRequest } from "next/server";
+// import {headers} from 'next/headers';
+
+export async function GET(request: NextRequest) {
+    const requestHeaders = new Headers(request.headers);
+    // const headerList = headers();
+
+    const theme = request.headers.get('Cookie');
+    console.log(requestHeaders.get('Authorization')); //put in the name of the header
+    // console.log(headerList.get('Authorization')); //put in the name of the header
+    console.log(theme);
+
+    return new Response('<h1>Profile data<h1>',{
+    headers: {
+        'Content-Type': 'text/html',
+        'set-cookie': 'theme=dark',
+    },}
+    );
 }
